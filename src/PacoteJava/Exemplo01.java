eb81e0646f3fe21d68095de3d8dfe7694eeb450e
milton felga <milton.felga@etec.sp.gov.br>
2019-08-13 18:59:43 -0300

    Projeto Java

---
+++ src/PacoteJava/Exemplo01.java
@@ -0,0 +1,16 @@
+//comentário de linha
+/*comentário 
+de bloco
+*/
+//Sistema de exemplo01
+package PacoteJava;
+
+public class Exemplo01 {
+    public static void main(String[] args) {
+        escreverNome(); //chamada de metodo
+    }
+    
+    public static void escreverNome(){ // criei um metodo
+        
+    }
+}
---
+++ src/PacoteJava/Exemplo02.java
@@ -0,0 +1,8 @@
+
+package PacoteJava;
+
+public class Exemplo02 {
+    public static void main(String[] args) {
+        Exemplo01.escreverNome();
+    }
+}
